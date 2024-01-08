72323581db2b2442f1e6aa9e5586c44758f2fc9e
nurgat <142540895+nurgat@users.noreply.github.com>
2024-01-08 20:39:22 +0400

    style.css

--- wp-content/themes/BlankSlate-1701617353-via-wpmarmite/functions.php
+++ wp-content/themes/BlankSlate-1701617353-via-wpmarmite/functions.php
@@ -6,13 +6,20 @@ function wpchild_enqueue_styles(){
   wp_enqueue_style( 'wpm-BlankSlate-style', get_template_directory_uri() . '/style.css' );
 }
 
-add_filter( 'wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2 );
-function add_admin_link_to_menu( $items, $args ) {
-    if (is_user_logged_in() && $args->menu == 'main-menu') {
-        $items .= '<li><a href="'. get_permalink( get_option('woocommerce_myaccount_page_id') ) .'">My Account</a></li>';
+// Ajoute un filtre pour modifier les éléments du menu
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin_au_menu', 10, 2);
+
+// Fonction appelée lors du filtrage des éléments du menu
+function ajouter_lien_admin_au_menu($items, $args) {
+    // Vérifie si l'utilisateur est connecté
+    if (is_user_logged_in()) {
+        // Vérifie si le menu actuel est le menu principal
+        $menu_principal = 'Menu Header (Main Menu)';
+        if ($args->theme_location == $menu_principal) {
+            // Ajoute le lien "Admin" au menu principal
+            $items .= '<li>' . wp_loginout(home_url('/wp-admin/'), false) . '</li>';
+        }
     }
-    elseif (!is_user_logged_in() && $args->menu == 'main-menu') {
-        $items .= '<li><a href="' . get_permalink( wc_get_page_id( 'myaccount' ) ) . '">Sign in  /  Register</a></li>';
-    }
+
     return $items;
 }
